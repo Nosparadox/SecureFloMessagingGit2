fd627bb18aff78608d62177b6ddc4e0a90a6df72
Brandon Lyssy <Brandon Lyssy>
2016-10-26 19:47:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nos/secureflomessaging/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.example.nos.secureflomessaging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
