7f657486ca3370a99444ca1b8f0ad2ffc84b2883
Nosparadox <bue135@my.utsa.edu>
2016-11-30 02:19:14 -0600

    Changed splash.xml old: android:gravity = "fill" new: android:gravity = "center_horizontal"

--- app/src/main/java/com/example/nos/secureflomessaging/SplashScreen.java
+++ app/src/main/java/com/example/nos/secureflomessaging/SplashScreen.java
@@ -12,7 +12,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, LoginRegisterActivity.class);
         startActivity(intent);
         finish();
     }
